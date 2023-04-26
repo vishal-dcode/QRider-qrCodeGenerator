6352d3a4fb3a951086d472167b63e4f95d55b6f2
Vishal Vishwakarma <vishalvish4225@gmail.com>
2023-04-26 17:43:48 +0530

    Uploaded Project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# QRider-qrCodeGenerator
- QRider's mission is to meet people around the world and help them to understand and use QR Codes in their daily lives.
+# QRider - QR Code Generator
+
+QRider is a simple web app that generates QR codes from URLs using the QRCode library. The app is built with HTML, CSS, and JavaScript, and it also includes a loading spinner and a save button to download the generated QR code as an image.
+
+## Demo
+
+Check out the [demo page](https://your-username.github.io/qr-code-generator/) to see the app in action.
+![Alt text](https://i.ibb.co/YtdB3KN/QRider-preview-2.png)
+
+## Getting Started
+
+To use the app, simply enter a URL and a size for the QR code, and click the "Generate" button. The app will generate the QR code and display it in the container below, along with a save button that you can use to download the QR code as an image.
+
+## Usage
+
+1. Enter a URL in the input field.
+2. Choose a size for the QR code.
+3. Click the "Generate" button to generate the QR code.
+4. Once the QR code is generated, a download button will appear that allows you to download the QR code as an image.
+
+## Contributing
+
+Contributions to this project are welcome and encouraged! If you find a bug or have an idea for a new feature, please open an issue or submit a pull request.
+
+To submit a pull request:
+
+1. Fork the repository.
+2. Create a new branch for your changes.
+3. Make your changes and commit them.
+4. Push your changes to your forked repository.
+5. Open a pull request from your branch to the `main` branch of the main repository.
+
+If you find this project useful, please consider giving it a star.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ app.js
@@ -0,0 +1,79 @@
+// Get the generate form and the QR code container
+const form = document.getElementById("generate-form"),
+  qr = document.getElementById("qrcode");
+
+// Handle form submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault(); // Prevent default form submission behavior
+
+  clearUI(); // Clear previous QR code and save button (if any)
+
+  // Get URL and size input values
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  // Validate URL input
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner(); // Show loading spinner
+
+    // Wait for 1.5 seconds before generating QR code (to simulate a delay)
+    setTimeout(() => {
+      hideSpinner(); // Hide loading spinner
+
+      // Generate QR code and show save button after a 50ms delay
+      generateQRCode(url, size);
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src; // Get QR code image source URL
+        createSaveBtn(saveUrl); // Create save button to download QR code as image
+      });
+    }, 1500);
+  }
+};
+
+// Generate QR code using the QRCode library
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = ""; // Clear QR code container
+  const saveBtn = document.getElementById("saveLink"); // Check if save button exists
+  if (saveBtn) {
+    const parentElement = saveBtn.parentElement; // Select parent element of saveBtn
+    parentElement.remove(); // Remove save button (if it exists)
+  }
+};
+
+// Show loading spinner
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+
+// Hide loading spinner
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
+};
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const btn = document.createElement("button"); // Create button element
+  const link = document.createElement("a"); // Create link element
+  link.id = "saveLink"; // Set link ID for future reference
+  link.href = saveUrl; // Set download URL
+  link.download = "qrcode"; // Set download filename
+  link.innerHTML = "Download QR Code"; // Set link text content
+  btn.appendChild(link); // Add link to button
+  document.getElementById("generated").appendChild(btn); // Add button to parent container
+};
+
+hideSpinner(); // Hide loading spinner by default
+
+// Add event listener for form submit
+form.addEventListener("submit", onGenerateSubmit);
